18e80cb5c6bc3a439af18825bc448ab6d57264ef
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2025-02-08 00:11:51 +0100

    Added wpf controls project. Added ExtendedTextBox.

---
+++ Source/Scotec.Wpf.Controls/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+#endregion
+
+namespace Scotec.Wpf.Converter;
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public class BooleanToVisibilityConverter : IValueConverter
+{
+    public BooleanToVisibilityConverter()
+    {
+        TrueVisibility = Visibility.Visible;
+        FalseVisibility = Visibility.Collapsed;
+    }
+
+    public Visibility TrueVisibility { get; set; }
+    public Visibility FalseVisibility { get; set; }
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (!(value is bool boolValue))
+        {
+            return FalseVisibility;
+        }
+
+        if (parameter != null && parameter.ToString() == "Invert")
+        {
+            return boolValue ? FalseVisibility : TrueVisibility;
+        }
+
+        return boolValue ? TrueVisibility : FalseVisibility;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
